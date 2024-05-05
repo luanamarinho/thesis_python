9d312939aa0deb4545e2cbdf6da8865ba8dd2203
luanamarinho <luana.marinho@gmail.com>
2024-05-05 15:32:50 +0200

    update run pipelines VM

--- run_pipelines.py
+++ run_pipelines.py
@@ -4,28 +4,34 @@ from utils.run_opentsne import run_openTSNE_with_combinations
 from joblib import dump, load
 import gzip
 import numpy as np
+import time
 
 # Configure logging
-logging.basicConfig(filename='/mnt/cs-luana-marinho-gmail-com-100320037b715e57/script.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='/mnt/cs-luana-marinho-gmail-com-100320037b715e57/log/script.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
     try:
         # Load data
-        data_file_path = '/mnt/cs-luana-marinho-gmail-com-100320037b715e57/originaldata_HGV_preproc_gzip'
+        data_file_path = '/mnt/cs-luana-marinho-gmail-com-100320037b715e57/data_preprocessed_40000_10HVG'
         data_file = gzip.GzipFile(data_file_path, "r"); expr_data_preprocessed = np.load(data_file)
-        expr_data_preprocessed_sampled = expr_data_preprocessed[np.random.choice(expr_data_preprocessed.shape[0], size=20000, replace=False)]
+        np.random.seed(1234)
+        ind_to_sample = np.random.choice(expr_data_preprocessed.shape[0], size=2000, replace=False)
+        expr_data_preprocessed_sampled = expr_data_preprocessed[ind_to_sample]
 
         # Compute affinities
         #np.linspace(5, 90, num=18, dtype=int).tolist()
-        combination = [(80, 4, 0.1, 0.8, 0.5)]
+        combination = [(5, 4, 0.1, 0.8, 0.5)]
+        start_time= time.time()
         affinity_cache = compute_affinities(X=expr_data_preprocessed_sampled,
-                                            perplexity_values=[80])
+                                            perplexity_values=[5])
         pipeline = run_openTSNE_with_combinations(combination, X = expr_data_preprocessed_sampled, affinity_cache = affinity_cache, verbose=True)
+        runtime_pipeline = time.time() - start_time
         
         # Save results
+        output = (pipeline, runtime_pipeline)
         combination_str = '_'.join(map(str, combination[0]))
-        result_file_path = f'/mnt/cs-luana-marinho-gmail-com-100320037b715e57/pipeline_{combination_str}.joblib'
-        dump(pipeline, result_file_path)
+        result_file_path = f'/mnt/cs-luana-marinho-gmail-com-100320037b715e57/output/pipeline_{combination_str}.joblib'
+        dump(output, result_file_path)
 
         logging.info("Script executed successfully.")
 
