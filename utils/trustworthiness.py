e0c7640d264cb94d9850095ec4ce4ab7b7073074
luanamarinho <luana.marinho@gmail.com>
2024-06-02 23:21:17 +0200

    trustworthiness with precomputed dist matrices

--- utils/trustworthiness.py
+++ utils/trustworthiness.py
@@ -1,28 +1,32 @@
 from sklearn.metrics import pairwise_distances
-from sklearn.neighbors import NearestNeighbors
 import numpy as np
 
-def trustworthiness_ratio(X, X_embedded, min_k = 1, max_k = 70, metric = 'precomputed', digits = 3):
+def trustworthiness_ratio(dist_X, dist_embedded, min_k = 1, max_k = 70, metric = 'precomputed'):
     """
     
     Examples
     --------
     >>> from sklearn.manifold import TSNE
+    >>> from sklearn.metrics import pairwise_distances
+    >>> from sklearn.manifold import trustworthiness
     >>> X = np.random.RandomState(42).random((100, 50))  # Example high-dimensional data
     >>> X_embedded = TSNE(n_components=2, random_state=42).fit_transform(X)
     >>> dist_X = pairwise_distances(X, metric='euclidean')
-    >>> print(trustworthiness_ratio(dist_X, X_embedded, min_k=1, max_k=10, metric='precomputed'))
+    >>> dist_embedded = pairwise_distances(X_embedded, metric='euclidean')
+    >>> print(trustworthiness_ratio(dist_X, dist_embedded, min_k=1, max_k=10, metric='precomputed'))
     ([0.9638775510204082, 0.9106217616580311, 0.891859649122807, 0.8721122994652406, 0.8451086956521738, 0.8327071823204419, 0.8154253611556983, 0.8084285714285715, 0.7922093023255814, 0.7808994082840237], 0.839)
+    >>> trustworthiness(X, X_embedded, n_neighbors=5)
+    0.8451086956521738
     """
     T_k = []
-    n_samples = X.shape[0]
+    n_samples = dist_X.shape[0]
     if max_k >= n_samples / 2:
       raise ValueError(
         f"k ({max_k}) should be less than n_samples / 2"
         f" ({n_samples / 2})"
       )
     
-    dist_X = pairwise_distances(X, metric=metric)
+    dist_X = pairwise_distances(dist_X, metric=metric)
     if metric == "precomputed":
         dist_X = dist_X.copy()
     # we set the diagonal to np.inf to exclude the points themselves from
@@ -30,11 +34,11 @@ def trustworthiness_ratio(X, X_embedded, min_k = 1, max_k = 70, metric = 'precom
     np.fill_diagonal(dist_X, np.inf)
     ind_X = np.argsort(dist_X, axis=1)
     # `ind_X[i]` is the index of sorted distances between i and other samples
-    ind_X_embedded = (
-        NearestNeighbors(n_neighbors=max_k)
-        .fit(X_embedded)
-        .kneighbors(return_distance=False)
-    )
+   
+    dist_embedded = pairwise_distances(dist_embedded, metric=metric)
+    if metric == "precomputed":
+        dist_embedded = dist_embedded.copy()
+    ind_X_embedded = np.argsort(dist_embedded, axis=1)[:,1:]
 
     # We build an inverted index of neighbors in the input space: For sample i,
     # we define `inverted_index[i]` as the inverted index of sorted distances:
@@ -57,7 +61,7 @@ def trustworthiness_ratio(X, X_embedded, min_k = 1, max_k = 70, metric = 'precom
     median_first_25_percent = np.median(T_k[:int(0.25*max_k)])
     start_index = max_k - int(0.25*max_k)
     median_last_25_percent = np.median(T_k[start_index:])
-    trustworthiness_ratio = round(median_last_25_percent/median_first_25_percent, ndigits=digits)
-    return (T_k, trustworthiness_ratio)
+    trustworthiness_ratio = median_last_25_percent/median_first_25_percent
+    return T_k, trustworthiness_ratio
 
 
