a0d511f756259ab51ca613e59259e64f8155c864
luanamarinho <luana.marinho@gmail.com>
2025-03-27 08:32:44 +0100

    improve doc

--- utils/parameters_combination_granular.py
+++ utils/parameters_combination_granular.py
@@ -4,23 +4,25 @@ def generate_combinations(perplexity_range = (5, 150),
                           theta_range = (0, 1.0),
                           initial_momem_range = (0.1, 0.5),
                           final_momem_range = (0.8, 1),
-                          num_combinations = 600):
+                          num_combinations = 600,
+                          seed = 42):
     """
     Generate combinations of parameters based on specified ranges.
 
     Parameters:
+    - perplexity_range: Tuple containing the start and stop values for perplexity range.
     - early_exagg_range: Tuple containing the start and stop values for early_exagg range.
     - theta_range: Tuple containing the start and stop values for theta range.
-    - perplexity: single integer for the algorithm perplexity. Should be one of [5, 25, 45, 65, 90]
+    - initial_momem_range: Tuple containing the start and stop values for initial_momem range.
+    - final_momem_range: Tuple containing the start and stop values for final_momem range.
+    - num_combinations: Number of combinations to generate.
+    - seed: Seed for the random number generator.
 
     Returns:
-    - A list of parameter combinations.
+    - A list of tuples with random combinations of the 5 Barnes-Hut-SNE parameters.
     """
-    
-    #if perplexity not in [5, 25, 45, 65, 90]:
-        #raise ValueError("Perplexity must be one of [5, 25, 45, 65, 90]")
 
-    np.random.seed(42)
+    np.random.seed(seed)
 
     perplexity_values = np.linspace(*perplexity_range, num=num_combinations//5).tolist()
     early_exagg_values = np.linspace(*early_exagg_range, num=num_combinations//5).tolist()
@@ -40,13 +42,3 @@ def generate_combinations(perplexity_range = (5, 150),
         combinations.add((perplexity, early_exagg, initial_momentum, final_momentum, theta))
 
     return list(combinations)
-
-
-# Convert the combinations to a DataFrame
-#df = pd.DataFrame(combinations, columns=['Perplexity', 'Early_Exaggeration', 'Initial_Momentum', 'Final_Momentum', 'Theta'])
-
-# Export the DataFrame to a CSV file
-#df.to_csv('output/tsne_parameter_combinations.csv', index=False)
-
-# Compute the correlation matrix
-#correlation_matrix = df.corr()
